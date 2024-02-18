42ad07beef581cddc0cf50c512e7cadac0229349
Shahanad <shahanadp21@gmail.com>
2024-02-18 14:41:15 +0530

    doing some changes

---
+++ lib/view/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_crud_app/view/employee.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => EmployeeScreen()));
+        },
+        child: Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        title: Text(
+          'Firebase Crud',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+    );
+  }
+}
