e212a5116d797a319b56e24987c2c95f06048ff4
Shahanad <shahanadp21@gmail.com>
2024-02-27 10:46:43 +0530

    project completed almost

--- lib/controller/base_provider.dart
+++ lib/controller/base_provider.dart
@@ -1,28 +1,4 @@
-// import 'package:flutter/material.dart';
-
-// class AllProvider extends ChangeNotifier {
-//   TextEditingController nameController = TextEditingController();
-//   TextEditingController ageController = TextEditingController();
-//   TextEditingController classController = TextEditingController();
-//   TextEditingController numberController = TextEditingController();
-//   TextEditingController addressController = TextEditingController();
-// }
-// // import 'dart:io';
-// // import 'package:flutter/foundation.dart';
-// // import 'package:image_picker/image_picker.dart';
-
-// // class BaseProvider extends ChangeNotifier {
-// //   File? selectedImage;
-// //   ImagePicker imagePicker = ImagePicker();
-// //   void setImage(ImageSource source) async {
-// //     final pickedImage = await imagePicker.pickImage(source: source);
-
-// //     selectedImage = pickedImage != null ? File(pickedImage.path) : null;
-// //     notifyListeners();
-// //   }
-// // }
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
--- lib/model/student_model.dart
+++ lib/model/student_model.dart
@@ -1,38 +1,3 @@
-// class StudentModel {
-//   String? name;
-//   String? age;
-//   String? studentClass;
-//   String? address;
-//   String? image;
-//   // int? id;
-
-//   StudentModel({
-//     this.name,
-//     this.age,
-//     this.studentClass,
-//     this.address,
-//     required this.image,
-//     // this.id,
-//   });
-//   factory StudentModel.fromJson(Map<String, dynamic> json) {
-//     return StudentModel(
-//       image: json['image'],
-//       name: json['name'] as String?,
-//       age: json['age'] as String?,
-//       studentClass: json['class'] as String?,
-//       address: json['address'] as String?,
-//     );
-//   }
-//   Map<String, dynamic> toJson() {
-//     return {
-//       'Image': image,
-//       'name': name,
-//       'age': age,
-//       'class': studentClass,
-//       'address': address,
-//     };
-//   }
-// }
 class StudentModel {
   String? name;
   String? rollno;
